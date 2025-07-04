fee239954ef5f184f586ba4209220bc99203fd05
Victor Cortez <vicortezdev@gmail.com>
2025-07-04 11:38:19 -0300

    update docs

--- part 1/1.1 - 1.5 application/README.md
+++ part 1/1.1 - 1.5 application/README.md
@@ -1,1 +1,2 @@
 Questions 1.1 - 1.5 Create and evolve a simple app to showcase some basic react functionality.
+![alt text](image-1.png)
--- part 1/1.1 - 1.5 application/my-app/src/components/ClassComponent.jsx
+++ part 1/1.1 - 1.5 application/my-app/src/components/ClassComponent.jsx
@@ -3,7 +3,7 @@ import React from "react";
 class ClassComponent extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { initial: true };
+    this.state = { val: true };
   }
 
   componentDidMount() {
@@ -12,9 +12,9 @@ class ClassComponent extends React.Component {
 
   handleClick() {
     this.setState(
-      (prevState) => ({ initial: !prevState.initial }),
+      (prevState) => ({ val: !prevState.val }),
       () => {
-        console.log("new state: " + this.state.initial);
+        console.log("new state: " + this.state.val);
       }
     );
   }
@@ -22,7 +22,9 @@ class ClassComponent extends React.Component {
   render() {
     return (
       <div>
+        <div>some tests, not related to the exercise</div>
         <div>hi: {this.props.a}</div>
+        <div>state: {String(this.state.val)}</div>
         <button onDoubleClick={() => this.handleClick()}> CLICK</button>
       </div>
     );
