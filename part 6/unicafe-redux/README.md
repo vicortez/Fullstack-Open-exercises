989e6500946c686e73c5edac0a481ccc129f091f
Victor Cortez <vicortezdev@gmail.com>
2025-12-08 15:53:13 -0300

    solution to exercises 6.1 to 6.2

---
+++ part 6/unicafe-redux/README.md
@@ -0,0 +1,5 @@
+# Part 6: Unicafe redux
+
+This project was initially seeded with the code at [https://github.com/fullstack-hy2020/unicafe-redux](https://github.com/fullstack-hy2020/unicafe-redux).
+
+Its supposed to be an alternative version of the unicafe app we made in part 1, but using Redux for state management.
---
+++ part 6/unicafe-redux/src/main.jsx
@@ -0,0 +1,31 @@
+import ReactDOM from 'react-dom/client'
+import { createStore } from 'redux'
+import counterReducer from './reducers/counterReducer'
+
+const store = createStore(counterReducer)
+
+const App = () => {
+  return (
+    <div>
+      <button onClick={() => store.dispatch({ type: 'GOOD' })}>good</button>
+      <button onClick={() => store.dispatch({ type: 'OK' })}>ok</button>
+      <button onClick={() => store.dispatch({ type: 'BAD' })}>bad</button>
+      <button onClick={() => store.dispatch({ type: 'RESET' })}>reset stats</button>
+      <div>good {store.getState().good}</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const renderApp = () => {
+  root.render(<App />)
+}
+
+// run once to display app initially
+renderApp()
+
+// register as cb handler to re-render every time there is an update to the store
+store.subscribe(renderApp)
